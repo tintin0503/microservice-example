d0a0e70437ca4638f668cafa5001e5faf672a796
tintin0503 <quanptit2801@gmail.com>
2023-10-27 21:18:20 +0700

    example WebClient

--- inventory-service/src/main/java/com/tintin/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/tintin/inventoryservice/service/InventoryService.java
@@ -1,10 +1,13 @@
 package com.tintin.inventoryservice.service;
 
+import com.tintin.inventoryservice.dto.InventoryResponse;
 import com.tintin.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +16,14 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCodes) {
+        return inventoryRepository.findBySkuCodeIn(skuCodes).stream()
+                .map(inventory ->
+                    InventoryResponse.builder()
+                            .skuCode(inventory.getSkuCode())
+                            .isInStock(inventory.getQuantity() > 0)
+                            .build()
+                )
+                .toList();
     }
 }
