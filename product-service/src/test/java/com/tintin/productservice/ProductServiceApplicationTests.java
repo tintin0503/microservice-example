969934e59d6e98f43c9bfacb7ddac2babaf96e77
tintin0503 <quanptit2801@gmail.com>
2023-10-28 13:45:48 +0700

    add config security OAuth

--- product-service/src/test/java/com/tintin/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/tintin/productservice/ProductServiceApplicationTests.java
@@ -25,57 +25,57 @@ import org.testcontainers.utility.DockerImageName;
 import java.math.BigDecimal;
 
 @SpringBootTest
-//@Testcontainers
-//@AutoConfigureMockMvc
+@Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
-//
-//	@Container
-//	static MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
-//
-//	@Autowired
-//	private MockMvc mockMvc;
-//
-//	@Autowired
-//	private ObjectMapper objectMapper;
-//
-//	@Autowired
-//	private ProductRepository productRepository;
-//
-//	static {
-//		mongoDBContainer.start();
-//	}
-//
-//	@DynamicPropertySource
-//	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
-//		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
-//	}
-//
-//	@Test
-//	void shouldCreateProduct() throws Exception {
-//
-//		ProductRequest productRequest = getProductRequest();
-//		String productRequestString = objectMapper.writeValueAsString(productRequest);
-//
-//		mockMvc.perform(MockMvcRequestBuilders.post("/api/products")
-//				.contentType(MediaType.APPLICATION_JSON)
-//				.content(productRequestString)
-//		).andExpect(MockMvcResultMatchers.status().isCreated());
-//        Assertions.assertEquals(1, productRepository.findAll().size());
-//	}
-//
-//	@Test
-//	void shouldGetProducts() throws Exception {
-//		mockMvc.perform(MockMvcRequestBuilders.get("/api/product"))
-//				.andExpect(MockMvcResultMatchers.status().isOk());
-//	}
-//
-//
-//	private ProductRequest getProductRequest() {
-//		return ProductRequest.builder()
-//				.name("Iphone 13")
-//				.description("Iphone 13")
-//				.price(BigDecimal.valueOf(1200))
-//				.build();
-//	}
+
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	static {
+		mongoDBContainer.start();
+	}
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
+
+	@Test
+	void shouldCreateProduct() throws Exception {
+
+		ProductRequest productRequest = getProductRequest();
+		String productRequestString = objectMapper.writeValueAsString(productRequest);
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(productRequestString)
+		).andExpect(MockMvcResultMatchers.status().isCreated());
+        Assertions.assertEquals(1, productRepository.findAll().size());
+	}
+
+	@Test
+	void shouldGetProducts() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/product"))
+				.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+
+
+	private ProductRequest getProductRequest() {
+		return ProductRequest.builder()
+				.name("Iphone 13")
+				.description("Iphone 13")
+				.price(BigDecimal.valueOf(1200))
+				.build();
+	}
 
 }
