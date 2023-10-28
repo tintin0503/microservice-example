b2a0f490387702a56b0d3d38bdf2329ced0f9b8f
tintin0503 <quanptit2801@gmail.com>
2023-10-28 15:45:49 +0700

    config circuit breaker and resilience4j

--- inventory-service/src/main/java/com/tintin/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/tintin/inventoryservice/service/InventoryService.java
@@ -3,6 +3,8 @@ package com.tintin.inventoryservice.service;
 import com.tintin.inventoryservice.dto.InventoryResponse;
 import com.tintin.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import org.springframework.transaction.annotation.Transactional;
@@ -11,12 +13,18 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCodes) {
+        log.info("Wait Started");
+        Thread.sleep(10000);
+        log.info("Wait ended");
+
         return inventoryRepository.findBySkuCodeIn(skuCodes).stream()
                 .map(inventory ->
                     InventoryResponse.builder()
